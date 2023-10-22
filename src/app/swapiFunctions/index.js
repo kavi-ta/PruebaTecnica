2baf8c9c92646cc84eb5c0d6908102b2b83fd193
kavi-ta <kavisharma2807@gmail.com>
2023-10-22 17:04:38 +0530

    feat(swapiFunctions): Error handling added to handle the fetching of invalid id on swapi

--- src/app/swapiFunctions/index.js
+++ src/app/swapiFunctions/index.js
@@ -13,6 +13,11 @@ const genericRequest = async (url, method, body, logging = false) => {
     }
     const response = await fetch(url, options);
     const data = await response.json();
+    if (response.status===404){
+        // incase an invalid id is passed, throw an error
+        throw new Error("Details not found");
+    }
+    
     if(logging){
         console.log(data);
     }
