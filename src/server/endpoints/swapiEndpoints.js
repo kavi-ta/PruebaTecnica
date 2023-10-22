1a5744844daf93cec96f6f1d91f057021ffe4bd3
kavi-ta <kavisharma2807@gmail.com>
2023-10-22 13:26:29 +0530

    refactor(swapiEndpoints): implemented task separation by adding a service layer and endpoints returning only the response

--- src/server/endpoints/swapiEndpoints.js
+++ src/server/endpoints/swapiEndpoints.js
@@ -1,3 +1,6 @@
+// service imports
+const swapiGetPeopleById = require("../../app/service/people")
+const swapiGetPlanetById = require("../../app/service/planet")
 
 const _isWookieeFormat = (req) => {
     if(req.query.format && req.query.format == 'wookiee'){
@@ -16,104 +19,19 @@ const applySwapiEndpoints = (server, app) => {
     });
 
     server.get('/hfswapi/getPeople/:id', async (req, res) => {
-        const {id} = req.params
-        // fetch person by id from db
-        let person = await app.db.swPeople.findOne({where: {id:id}})
-        if(!person){
-            // if person doesnt exist in db, get it from the swapi.dev
-            person = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/people/${id}`, 'GET', null, true);
-            // fetch the planet of the person from swapi.dev
-            const planet = await app.swapiFunctions.genericRequest(person.homeworld, 'GET', null, true)
-            // check if planet exists in db
-            let homeworldPlanet = await app.db.swPlanet.findOne({where : {name: planet.name}})
-            if(!homeworldPlanet){
-                // if planet doesnt exist in db, add it to db
-                await app.db.swPlanet.create({
-                    name: planet.name,
-                    gravity: parseInt(planet.gravity.split(" ")[0])
-                })
-                homeworldPlanet = await app.db.swPlanet.findOne({where : {name: planet.name}})
-            }
-            // add the person to db
-            await app.db.swPeople.create({
-                name: person.name,
-                mass: person.mass,
-                height: person.height,
-                homeworld_name: homeworldPlanet.name,
-                homeworld_id: `/planets/${homeworldPlanet.id}`,
-            })
-
-            person = await app.db.swPeople.findOne({where: {id:id}})
-        }
-        res.send({
-            "name": person?.name,
-            "mass": person?.mass,
-            "height":person?.height,
-            "homeworldName": person?.homeworld_name,
-            "homeworldId": person?.homeworld_id
-        })
-        
+        const {id} = req.params;
+        const swPerson = await swapiGetPeopleById(parseInt(id));
+        res.send(swPerson);
     });
 
     server.get('/hfswapi/getPlanet/:id', async (req, res) => {
-        const {id} = req.params
-        // fetch planet from db
-        let planet = await app.db.swPlanet.findOne({where : {id: id}})
-        
-        if(!planet){
-            const planetData = await app.swapiFunctions.genericRequest(`${swGenericApi}/planets/${id}`, 'GET', null, true)
-            await app.db.swPlanet.create({
-                name: planetData.name,
-                gravity: parseInt(planetData.gravity.split(" ")[0])
-            })
-            planet = await app.db.swPlanet.findOne({where : {name: planetData.name}})
-        }
-
-        res.send({
-            "name": planet.name,
-            "gravity": planet.gravity
-        });
+        const {id} = req.params;
+        const swPlanet = await swapiGetPlanetById(parseInt(id));
+        res.send(swPlanet);
     });
 
     server.get('/hfswapi/getWeightOnPlanetRandom', async (req, res) => {
-        const randomPeopleId = 1
-        const randomPlanetId = 5
-        const randomPeople = await app.db.swPeople.findOne({where: {id:randomPeopleId}})
-        if(!randomPeople){
-            data = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/people/${randomPeopleId}`, 'GET', null, true);
-            const planet = await app.swapiFunctions.genericRequest(data.homeworld, 'GET', null, true)
-            let homeworldPlanet = await app.db.swPlanet.findOne({where : {name: planet.name}})
-            if(!homeworldPlanet){
-
-                await app.db.swPlanet.create({
-                    name: planet.name,
-                    gravity: parseInt(planet.gravity.split(" ")[0])
-                })
-                homeworldPlanet = await app.db.swPlanet.findOne({where : {name: planet.name}})
-            }
-            await app.db.swPeople.create({
-                name: data.name,
-                mass: data.mass,
-                height: data.height,
-                homeworld_name: homeworldPlanet.name,
-                homeworld_id: `/planets/${homeworldPlanet.id}`,
-            })
-
-            data = await app.db.swPeople.findOne({where: {id:id}})
-        }
-
-        let randomPlanet = await app.db.swPlanet.findOne({where: {id:randomPlanetId}})
-        if(!randomPlanet){
-            const planet = await app.swapiFunctions.genericRequest(`${swGenericApi}/planets/${randomPlanetId}`, 'GET', null, true)
-            await app.db.swPlanet.create({
-                name: planet.name,
-                gravity: parseInt(planet.gravity.split(" ")[0])
-            })
-            randomPlanet = await app.db.swPlanet.findOne({where : {name: planet.name}})
-        }
-
-
-        res.send({"weight": randomPeople.mass * randomPlanet.gravity});
+        res.send({"message": "To be implemented"});
     });
 
     server.get('/hfswapi/getLogs',async (req, res) => {
